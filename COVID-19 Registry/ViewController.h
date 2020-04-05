e5ae6e1e00e66f8d773fe8a529538dae17ffc833
Greg Fiumara <greg@gregfiumara.com>
2020-04-05 11:58:12 -0400

    Nicer navigation.

--- COVID-19 Registry/ViewController.h
+++ COVID-19 Registry/ViewController.h
@@ -1,9 +1,10 @@
 // SPDX-License-Identifier: CC0-1.0
 
+@import SafariServices;
 @import ResearchKit;
 @import UIKit;
 
-@interface ViewController : UIViewController <ORKTaskViewControllerDelegate>
+@interface ViewController : UIViewController <ORKTaskViewControllerDelegate, UITableViewDataSource, UITableViewDelegate, SFSafariViewControllerDelegate>
 
 
 @end
--- COVID-19 Registry/ViewController.m
+++ COVID-19 Registry/ViewController.m
@@ -1,5 +1,6 @@
 // SPDX-License-Identifier: CC0-1.0
 
+@import SafariServices;
 @import ResearchKit;
 
 #import "RCRRegistryEntryTask.h"
@@ -8,25 +9,192 @@
 
 @interface ViewController ()
 
+@property (weak, nonatomic) IBOutlet UITableView *tableView;
+
+typedef NS_ENUM(NSInteger, RCRMainScreenTableViewSection)
+{
+	RCRMainScreenTableViewSectionRegistry,
+	RCRMainScreenTableViewSectionOther,
+
+	RCRMainScreenTableViewNumberOfSections
+};
+
+typedef NS_ENUM(NSInteger, RCRMainScreenTableViewRegistrySectionItems)
+{
+	RCRMainScreenTableViewRegistrySectionItemAdult,
+	RCRMainScreenTableViewRegistrySectionItemPediatric,
+
+	RCRMainScreenTableViewRegistrySectionNumberOfItems
+};
+
+typedef NS_ENUM(NSInteger, RCRMainScreenTableViewOtherSectionItems)
+{
+	RCRMainScreenTableViewOtherSectionItemIRB,
+	RCRMainScreenTableViewOtherSectionItemAboutGRA,
+	RCRMainScreenTableViewOtherSectionItemAcknowledgements,
+
+	RCRMainScreenTableViewOtherSectionNumberOfItems
+};
+
 @end
 
 @implementation ViewController
 
-- (void)viewDidLoad {
-	[super viewDidLoad];
-	// Do any additional setup after loading the view.
+#pragma mark - Survey Delegate
+
+- (void)taskViewController:(nonnull ORKTaskViewController *)taskViewController didFinishWithReason:(ORKTaskViewControllerFinishReason)reason error:(nullable NSError *)error
+{
+	[taskViewController dismissViewControllerAnimated:YES completion:nil];
+	[self.tableView deselectRowAtIndexPath:[self.tableView indexPathForSelectedRow] animated:YES];
 }
 
-- (IBAction)getStartedPressed:(id)sender
+#pragma mark - Actions
+
+- (void)startNewAdultEntry
 {
 	ORKTaskViewController *taskViewController = [[ORKTaskViewController alloc] initWithTask:[RCRRegistryEntryTask new] taskRunUUID:nil];
 	taskViewController.delegate = self;
 	[self presentViewController:taskViewController animated:YES completion:nil];
 }
 
-- (void)taskViewController:(nonnull ORKTaskViewController *)taskViewController didFinishWithReason:(ORKTaskViewControllerFinishReason)reason error:(nullable NSError *)error
+#pragma mark - Table View
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
 {
-	[taskViewController dismissViewControllerAnimated:YES completion:nil];
+	switch ((RCRMainScreenTableViewSection)section) {
+		case RCRMainScreenTableViewSectionRegistry:
+			return (RCRMainScreenTableViewRegistrySectionNumberOfItems);
+		case RCRMainScreenTableViewSectionOther:
+			return (RCRMainScreenTableViewOtherSectionNumberOfItems);
+		case RCRMainScreenTableViewNumberOfSections:
+			return (0);
+	}
+}
+
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView
+{
+	return (RCRMainScreenTableViewNumberOfSections);
+}
+
+- (NSString *)tableView:(UITableView *)tableView titleForHeaderInSection:(NSInteger)section
+{
+	switch ((RCRMainScreenTableViewSection)section) {
+		case RCRMainScreenTableViewSectionRegistry:
+			return (NSLocalizedString(@"Registries", @""));
+		case RCRMainScreenTableViewSectionOther:
+			return (NSLocalizedString(@"Information", @""));
+		case RCRMainScreenTableViewNumberOfSections:
+			return (nil);
+	}
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+	static NSString *defaultCellReuseIdentifier = @"defaultCellReuseIdentifier";
+	UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:defaultCellReuseIdentifier];
+	if (cell == nil)
+		cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleSubtitle reuseIdentifier:defaultCellReuseIdentifier];
+
+	// Reset all
+	cell.textLabel.text = nil;
+	cell.textLabel.numberOfLines = 1;
+	cell.detailTextLabel.text = nil;
+	cell.accessoryType = UITableViewCellAccessoryNone;
+
+	switch ((RCRMainScreenTableViewSection)indexPath.section) {
+		case RCRMainScreenTableViewSectionRegistry:
+			switch ((RCRMainScreenTableViewRegistrySectionItems)indexPath.row) {
+				case RCRMainScreenTableViewRegistrySectionItemAdult:
+					cell.textLabel.text = NSLocalizedString(@"New Adult Registry Entry", nil);
+					cell.accessoryType = UITableViewCellAccessoryDisclosureIndicator;
+					break;
+				case RCRMainScreenTableViewRegistrySectionItemPediatric:
+					cell.textLabel.text = NSLocalizedString(@"New Pediatric Registry Entry", nil);
+					cell.detailTextLabel.text = @"Coming soon";
+					break;
+				case RCRMainScreenTableViewRegistrySectionNumberOfItems:
+					break;
+			}
+			break;
+		case RCRMainScreenTableViewSectionOther:
+			switch ((RCRMainScreenTableViewOtherSectionItems)indexPath.row) {
+				case RCRMainScreenTableViewOtherSectionItemIRB:
+					cell.textLabel.text = NSLocalizedString(@"IRB Information", nil);
+					cell.accessoryType = UITableViewCellAccessoryDisclosureIndicator;
+					break;
+				case RCRMainScreenTableViewOtherSectionItemAboutGRA:
+					cell.textLabel.text = NSLocalizedString(@"About the Global Rheumatology Alliance", nil);
+					cell.textLabel.numberOfLines = 0;
+					cell.accessoryType = UITableViewCellAccessoryDisclosureIndicator;
+
+					break;
+				case RCRMainScreenTableViewOtherSectionItemAcknowledgements:
+					cell.textLabel.text = NSLocalizedString(@"About C-19 Registry App", nil);
+					cell.detailTextLabel.text = @"Coming soon";
+					break;
+				case RCRMainScreenTableViewOtherSectionNumberOfItems:
+					break;
+			}
+			break;
+		case RCRMainScreenTableViewNumberOfSections:
+			break;
+	}
+
+	return (cell);
+}
+
+- (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath
+{
+	switch ((RCRMainScreenTableViewSection)indexPath.section) {
+		case RCRMainScreenTableViewSectionRegistry:
+			switch ((RCRMainScreenTableViewRegistrySectionItems)indexPath.row) {
+				case RCRMainScreenTableViewRegistrySectionItemAdult:
+					[self startNewAdultEntry];
+					break;
+				case RCRMainScreenTableViewRegistrySectionItemPediatric: {
+					UIAlertController *notYetImplementedAlert = [UIAlertController alertControllerWithTitle:@"Not Implemented" message:@"This feature has not yet been implemented." preferredStyle:UIAlertControllerStyleAlert];
+					[notYetImplementedAlert addAction:[UIAlertAction actionWithTitle:NSLocalizedString(@"Okay", @"") style:UIAlertActionStyleDefault handler:^(UIAlertAction * action) {[tableView deselectRowAtIndexPath:indexPath animated:YES];}]];
+					[self presentViewController:notYetImplementedAlert animated:YES completion:nil];
+					break;
+				}
+				case RCRMainScreenTableViewRegistrySectionNumberOfItems:
+					break;
+			}
+			break;
+		case RCRMainScreenTableViewSectionOther:
+			switch ((RCRMainScreenTableViewOtherSectionItems)indexPath.row) {
+				case RCRMainScreenTableViewOtherSectionItemIRB: {
+					SFSafariViewController *svc = [[SFSafariViewController alloc] initWithURL:[NSURL URLWithString:@"https://rheum-covid.org/irb/"]];
+					svc.delegate = self;
+					[self presentViewController:svc animated:YES completion:nil];
+					break;
+				}
+				case RCRMainScreenTableViewOtherSectionItemAboutGRA: {
+					SFSafariViewController *svc = [[SFSafariViewController alloc] initWithURL:[NSURL URLWithString:@"https://rheum-covid.org/about/"]];
+					svc.delegate = self;
+					[self presentViewController:svc animated:YES completion:nil];
+					break;
+				}
+				case RCRMainScreenTableViewOtherSectionItemAcknowledgements: {
+					UIAlertController *notYetImplementedAlert = [UIAlertController alertControllerWithTitle:@"Not Implemented" message:@"This feature has not yet been implemented." preferredStyle:UIAlertControllerStyleAlert];
+					[notYetImplementedAlert addAction:[UIAlertAction actionWithTitle:NSLocalizedString(@"Okay", @"") style:UIAlertActionStyleDefault handler:^(UIAlertAction * action) {[tableView deselectRowAtIndexPath:indexPath animated:YES];}]];
+					[self presentViewController:notYetImplementedAlert animated:YES completion:nil];
+					break;
+				}
+				case RCRMainScreenTableViewOtherSectionNumberOfItems:
+					break;
+			}
+			break;
+		case RCRMainScreenTableViewNumberOfSections:
+			break;
+	}
+}
+
+#pragma mark - SFSafariViewControllerDelegate
+
+- (void)safariViewControllerDidFinish:(SFSafariViewController *)controller
+{
+	[self.tableView deselectRowAtIndexPath:[self.tableView indexPathForSelectedRow] animated:YES];
 }
 
 @end
